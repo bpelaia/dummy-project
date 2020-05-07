d86e759b2395c3c4faf542f645616ce28e69ac44
Bruno Pelaia <brunopelaia@5minformatica.it>
2020-05-08 00:43:11 +0200

    dummy

--- src/main/java/vg/carlesi/Program.java
+++ src/main/java/vg/carlesi/Program.java
@@ -2,8 +2,12 @@ package vg.carlesi;
 
 public class Program {
 
-	public static void main(String[] args) {
+	private static String version = "v1.0.0.0";
+	private static String url = "http://localhost:8080";
+	private static String username = "dummy";
+	private static String password = "fake?U5er.P@ssWD";
 
+	public static void main(String[] args) {
 		System.out.println("I'm dummy.");
 	}
 
