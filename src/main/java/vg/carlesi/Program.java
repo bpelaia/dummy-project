5e64c5ec2067cb5709966961cae23b3d9c6f1755
Bruno Pelaia <bpelaia@users.noreply.github.com>
2021-02-18 15:54:27 +0100

    Risolta violazione SR xxxxxx

--- src/main/java/vg/carlesi/Program.java
+++ src/main/java/vg/carlesi/Program.java
@@ -2,7 +2,6 @@ package vg.carlesi;
 
 public class Program {
 
-	private static String version = "v1.0.0.0";
 	private static String url = "http://localhost:8080";
 	private static String username = "dummy";
 	private static String password = "fake?U5er.P@ssWD";
